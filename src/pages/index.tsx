2c5c400a21744c7d4ccafbf6af394fb4805a1e79
MaxBrych <max.brych03@gmail.com>
2023-04-20 08:29:57 +0200

    Posts in a grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ type Props = {
 export default function Home() {
   return (
     <main className="w-full min-h-screen bg-white">
-      <h1 className="my-6 text-4xl font-bold text-center text-black">Hi</h1>
       <Posts />
     </main>
   );
